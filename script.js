raunackt/naksatra-tracker

commit 3f4449e8def7c14a8d572c196c499cb18f945d09

feat: ist time based calculation

script.js
@@ -14,8 +14,11 @@ let loading = document.getElementById("loading");
 const DAY = 1000 * 60 * 60 * 24;
 
 function setNakshtra() {
-  // calculate how many day's have passed since 1th january, 2024 = ephemeris start date
-  let currentDate = new Date();
+  // calculate how many days have passed since 1st january, 2024 = ephemeris start date
+  let currentDate = convertGMTToIST(getGMTDate(new Date()));
+
+  console.log("Current IST Date: ", currentDate)
+
   let longToday = Math.floor((currentDate - new Date("2024-01-01")) / DAY);
   //get current time, calculate difference between NOW and sunrise and set delta accordingly
   let currentTime = new Date().getTime();
@@ -136,3 +139,28 @@ function setNakshtraName(rotateVal) {
 
 // call change function every 5 seconds
 setInterval(change, 1000 * 5);
+
+// converting date from any location to GMT
+function getGMTDate(date) {
+  // get the location offset from GMT in minutes
+  let currentTimezoneOffset = date.getTimezoneOffset();
+
+  // add/remove offset from the date's milliseconds
+  let gmt = date.getTime() + (currentTimezoneOffset * 60 * 1000);
+
+  // return new date based on GMT milliseconds
+  return new Date(gmt);
+}
+
+
+// convert GMT Date to IST Date
+function convertGMTToIST(gmtDate) {
+  // ISTOffset with respect to GMT is -330 minutes
+  // Since we have GMT Date, we need to add 330 minutes to convert GMT to IST
+  const ISTOffset = 330;
+
+  let istMillis = gmtDate.getTime() + (ISTOffset * 60 * 1000);
+
+  // return new date based on IST milliseconds
+  return new Date(istMillis);
+}

commit e85ef90c80b994835dd1b01152e3de8b4caf9a5a

script.js: Fix elapsed days calculation
Locale date object is used for calculation for number of days since
Jan 01 2024. But the comparison is with hacked date object to IST.

Fix that by comparing current unix timestamp to 01st Jan morning in
India.

Fixes: 3f4449e8def7 ("feat: ist time based calculation")

script.js
@@ -22,7 +22,8 @@ function setNakshtra() {
 
   console.log("Current IST Date: ", currentDate);
 
-  let longToday = Math.floor((currentDate - new Date("2024-01-01")) / DAY);
+  // 01st Jan 2024 06:55:00 IST = 1704072300000.
+  let longToday = Math.floor((Date().getTime() - 1704072300000) / DAY);
   //get current time, calculate difference between NOW and sunrise and set delta accordingly
   let currentTime = currentDate.getTime();
   let sunRiseForToday = new Date(